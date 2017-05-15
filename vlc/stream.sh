4adc4dfbcc1934fe54b6ff2654197aaf3cf26a0e
administrator <yacomelian@gmail.com>
2017-05-15 08:08:57 -0400

    Clean some comments

--- vlc/stream.sh
+++ vlc/stream.sh
@@ -1,1 +1,1 @@
-cvlc "rtmp://10.1.1.167/media_server/salajuntasa" --sout '#transcode{vcodec=theo,vb=800,scale=Automático,acodec=vorb,ab=64}:standard{access=http,mux=ogg,dst=:8080/}' --sout-keep
+cvlc "rtmp://x.x.x.x/media_server/*********" --sout '#transcode{vcodec=theo,vb=800,scale=Automático,acodec=vorb,ab=64}:standard{access=http,mux=ogg,dst=:8080/}' --sout-keep
